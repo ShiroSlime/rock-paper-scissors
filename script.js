b4cba12d23284c9e16c17da1d3a87ffc2e7c3509
ShiroSlime <158597353+ShiroSlime@users.noreply.github.com>
2024-02-24 11:44:08 +0530

    replaced while loop for results to an if statement
    while loop was unnecessary and if i forgot to increment the rounds(counter) it'd have ran infinitely
    also trying out prettier for code formatting

--- script.js
+++ script.js
@@ -1,66 +1,69 @@
 playGame();
 
 function getComputerChoice() {
-    let computerChoices = ["rock", "paper", "scissors"];
-    return computerChoices[Math.floor(Math.random() * computerChoices.length)];
+   let computerChoices = ["rock", "paper", "scissors"];
+   return computerChoices[Math.floor(Math.random() * computerChoices.length)];
 }
 
 function playRound(computerSelection, playerSelection) {
-    if (computerSelection === playerSelection) {
-        return "You tied!";
-    }
-    else if ((computerSelection === "rock" && playerSelection === "paper") ||
-        (computerSelection === "paper" && playerSelection === "scissors") ||
-        (computerSelection === "scissors" && playerSelection === "rock")) {
-        return "You won!!";
-    }
-    else if ((playerSelection === "rock" && computerSelection === "paper") ||
-        (playerSelection === "paper" && computerSelection === "scissors") ||
-        (playerSelection === "scissors" && computerSelection === "rock")) {
-        return ("You lost :(");
-    }
-    else {
-        return "Enter a valid choice";
-    }
+   if (computerSelection === playerSelection) {
+      return "You tied!";
+   } else if (
+      (computerSelection === "rock" && playerSelection === "paper") ||
+      (computerSelection === "paper" && playerSelection === "scissors") ||
+      (computerSelection === "scissors" && playerSelection === "rock")
+   ) {
+      return "You won!!";
+   } else if (
+      (playerSelection === "rock" && computerSelection === "paper") ||
+      (playerSelection === "paper" && computerSelection === "scissors") ||
+      (playerSelection === "scissors" && computerSelection === "rock")
+   ) {
+      return "You lost :(";
+   } else {
+      return "Enter a valid choice";
+   }
 }
 
 function playGame() {
-    let rounds = 0;
-    playerWin = 0,
-        computerWin = 0,
-        tie = 0;
+   let rounds = 0;
+   (playerWin = 0), (computerWin = 0), (tie = 0);
 
-    while (rounds < 5) {
-        let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
-        let computerSelection = getComputerChoice();
+   while (rounds < 5) {
+      let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+      let computerSelection = getComputerChoice();
 
-        if (playRound(computerSelection, playerSelection) === "You won!!") {
-            playerWin++;
-            console.log(playRound(computerSelection, playerSelection));
-        }
-        else if (playRound(computerSelection, playerSelection) === "You lost :(") {
-            computerWin++;
-            console.log(playRound(computerSelection, playerSelection));
-        }
-        else if (playRound(computerSelection, playerSelection) === "You tied!") {
-            tie++;
-            console.log(playRound(computerSelection, playerSelection));
-        }
-        else if (playRound(computerSelection, playerSelection) === "Enter a valid choice") {
-            console.log("Enter a valid choice");
-            rounds--;
-        }
-        rounds++;
-    }
+      if (playRound(computerSelection, playerSelection) === "You won!!") {
+         playerWin++;
+         console.log(playRound(computerSelection, playerSelection));
+      } else if (
+         playRound(computerSelection, playerSelection) === "You lost :("
+      ) {
+         computerWin++;
+         console.log(playRound(computerSelection, playerSelection));
+      } else if (
+         playRound(computerSelection, playerSelection) === "You tied!"
+      ) {
+         tie++;
+         console.log(playRound(computerSelection, playerSelection));
+      } else if (
+         playRound(computerSelection, playerSelection) ===
+         "Enter a valid choice"
+      ) {
+         console.log("Enter a valid choice");
+         rounds--;
+      }
+      rounds++;
+   }
 
-    while (rounds === 5) {
-        console.log(`The final score is :- your wins - ${playerWin}, computer wins - ${computerWin} and ${tie} ties.`);
-        if (playerWin > computerWin) {
-            console.log(`You won!!`);
-        }
-        else if (computerWin > playerWin) {
-            console.log(`You lost :(`);
-        }
-        rounds++;
-    }
+   if (rounds === 5) {
+      console.log(
+         `The final score is :- your wins - ${playerWin}, computer wins - ${computerWin} and ${tie} ties.`
+      );
+      if (playerWin > computerWin) {
+         console.log(`You won!!`);
+      } else if (computerWin > playerWin) {
+         console.log(`You lost :(`);
+      }
+   }
 }
